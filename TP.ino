6eed9586edeccedc2d8393c2c14f1a02a803041d
Hikary Muñoz Iida <hmunoz@fi.uba.ar>
2026-09-01 10:17:50 -0300

    TP.ino modificado para hacer pruebas aisladas de la IMU.

--- TP.ino
+++ TP.ino
@@ -1,30 +1,16 @@
-#include <NewPing.h>
-#include <Servo.h>
-
-#define PIN_POTE A0
-#define PIN_TRIG 7
-#define PIN_ECHO 6
-#define PIN_SERVO 5
-#define DISTANCIA_MAX 60 //distancia máxima (cm) que detecta sensor ultrasónico
-#define MICROS_EN_SEG 1000000.0
+// IMU
+#define MICROS_EN_SEG 1000000
 #define MICROS_50HZ 20000
-#define VEL_SONIDO 29.287 // us/cm
-#define SERVO_MIN 550 // us PWM duty cycle 
-#define SERVO_MIN 2400 // us PWM duty cycle 
+#define MICROS_100HZ 10000
 
 unsigned long t_inicio_loop; // Cuando inicia cada ciclo de tareas
 unsigned long t_loop_anterior; // Última ejecución de tareas
 unsigned long t_actual;
 
-NewPing sonar(PIN_TRIG, PIN_ECHO, DISTANCIA_MAX); 
-Servo servo;
-
-
 void setup() {
   Serial.begin(115200); // Suficientemente alto para que carguen los print
   t_inicio_loop = micros();
   t_loop_anterior = t_inicio_loop;
-  servo.attach(PIN_SERVO, SERVO_MIN, SERVO_MAX);
 }
 
 void loop() {
@@ -34,14 +20,8 @@ void loop() {
     t_loop_anterior = t_actual;
     t_inicio_loop += MICROS_50HZ;
 
-    // Ahora sí ejecuto tareas
-    int lectura_pote = analogRead(PIN_POTE);
-    float angulo = angulo_pote(lectura_pote);
-    //Serial.print("Ángulo: ");
-    //Serial.println(angulo);   
+    // ---------- Ahora sí ejecuto tareas ----------
 
-    distancia();
-    mover_servo_angulo(int(angulo));
   }
 }
 
@@ -50,31 +30,7 @@ void check_loop_freq(unsigned long t_inicio_loop, unsigned long t_fin_loop){
   float t_seg = t / MICROS_EN_SEG;
   float f = 1/t_seg;
   
-  //Serial.print("Frecuencia loop en Hz: ");
-  //Serial.println(f);
+  Serial.print("Frecuencia loop en Hz: ");
+  Serial.println(f);
 }
 
-float angulo_pote(int lectura_pote){
-  return lectura_pote * (270.0/1023.0);
-  }
-
-void distancia(){
-  unsigned int uS = sonar.ping(); // Tiempo de vuelo ida y vuelta
-  float distancia = uS/VEL_SONIDO;
-  //Serial.print("Ping: ");
-  //Serial.print(distancia); 
-  //Serial.println("cm")
-}
-
-void mover_servo_angulo(int angulo){
-  int aux = min(angulo, 200); // if value is < 200 it's treated as an angle, otherwise as pulse width in microseconds
-  //Serial.println(aux);
-  servo.write(aux);                  // sets the servo position according to the scaled value
-}
-
-void servo_180(){ // Tarda aprox 0.75 segundos en hacer 180°
-  servo.writeMicroseconds(SERVO_MIN);
-  delay(2000);
-  servo.writeMicroseconds(SERVO_MAX);
-  delay(2000);
-  }
